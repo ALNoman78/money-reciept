44ac9a4f64dcaa5b54ef33232be38e632dff195a
ALNoman78 <abdullaalno078@gmail.com>
2024-12-26 21:02:15 +0600

    project setup ready

---
+++ src/components/MoneyRecipt/MoneyReceipt.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const MoneyReceipt = () => {
+    return (
+        <div>
+            <h2>Money Receipt</h2>
+            <div>   
+                
+            </div>
+        </div>
+    )
+}
+
+MoneyReceipt.propTypes = {}
+
+export default MoneyReceipt
---
+++ src/components/Root/Root.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Root = props => {
+    return (
+        <div>
+            <h2 className="text-center">This is root components</h2>
+        </div>
+    )
+}
+
+Root.propTypes = {}
+
+export default Root
